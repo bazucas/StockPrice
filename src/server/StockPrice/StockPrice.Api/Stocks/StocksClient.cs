85d900249f61c9a1d43b45a8c7acf571de149030
Luís Inácio <lrs.inacio@gmail.com>
2024-10-18 21:32:57 +0100

    Stock client and service

---
+++ src/server/StockPrice/StockPrice.Api/Stocks/StocksClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace StockPrice.Api.Stocks;
+
+internal sealed class StocksClient(
+    HttpClient httpClient,
+    IConfiguration configuration,
+    IMemoryCache memoryCache,
+    ILogger<StocksClient> logger)
+{
+    public async Task<StockPriceResponse?> GetDataForTicker(string ticker)
+    {
+        logger.LogInformation("Getting stock price information for {Ticker}", ticker);
+
+        var stockPriceResponse = await memoryCache.GetOrCreateAsync($"stocks-{ticker}", async entry =>
+        {
+            entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+
+            return await GetStockPrice(ticker);
+        });
+
+        if (stockPriceResponse is null)
+        {
+            logger.LogWarning("Failed to get stock price information for {Ticker}", ticker);
+        }
+        else
+        {
+            logger.LogInformation(
+                "Completed getting stock price information for {Ticker}, {@Stock}",
+                ticker,
+                stockPriceResponse);
+        }
+
+        return stockPriceResponse;
+    }
+
+    private async Task<StockPriceResponse?> GetStockPrice(string ticker)
+    {
+        var tickerDataString = await httpClient.GetStringAsync(
+            $"?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=15min&apikey={configuration["Stocks:ApiKey"]}");
+
+        var tickerData = JsonConvert.DeserializeObject<AlphaVantageData>(tickerDataString);
+
+        var lastPrice = tickerData?.TimeSeries.FirstOrDefault().Value;
+
+        return lastPrice is null ? null : new StockPriceResponse(ticker, decimal.Parse(lastPrice.High, CultureInfo.InvariantCulture));
+    }
+}
